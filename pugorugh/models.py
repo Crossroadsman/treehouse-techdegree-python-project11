7251168d04863d76dc77650cd3d132e69e3b3517
CrossroadsMan <crossroadsman@gmail.com>
2019-02-01 10:33:56 -0700

    Create models

--- pugorugh/models.py
+++ pugorugh/models.py
@@ -1,3 +1,63 @@
-from django.db import models
+from django.conf import settings
+from django.db.models import Model, CASCADE
+from django.db.models import (CharField, PositiveIntegerField, ForeignKey)
 
-# Create your models here.
+
+class Dog(Model):
+    CHOICES = {
+        'GENDER': (
+            ('m', 'male'),
+            ('f', 'female'),
+            ('u', 'unknown'),
+        ),
+        'SIZE': (
+            ('s': 'small'),
+            ('m': 'medium'),
+            ('l': 'large'),
+            ('xl': 'extra large'),
+            ('u': 'unknown'),
+        ),
+    }
+
+    name = CharField(max_length=255)
+    # most Linux file systems are limited to 255 + 4096, Windows API: 260
+    image_filename = CharField(max_length=255)
+    breed = CharField(max_length=255)
+    age = PositiveIntegerField()  # months
+    gender = CharField(max_length=1, choices=CHOICES['GENDER'])
+    size = CharField(max_length=2, choices=CHOICES['SIZE'])
+
+
+class UserMTM(Model):
+    CHOICES = {
+        'STATUS': (
+            ('l', 'liked'),
+            ('d', 'disliked'),
+        ),
+        'AGE': (
+            ('b', 'baby'),
+            ('y', 'young'),
+            ('a', 'adult'),
+            ('s', 'senior'),
+        ),
+        'GENDER': (
+            ('m', 'male'),
+            ('f', 'female'),
+        ),
+        'SIZE': (
+            ('s', 'small'),
+            ('m', 'medium'),
+            ('l', 'large'),
+            ('xl', 'extra large'),
+        )
+    }
+
+    class Meta:
+        abstract = True
+
+
+class UserDog(UserMTM):
+
+    user = ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    dog = ForeignKey(to='Dog', on_delete=CASCADE)
+    status = CharField(max_length=1, choices=CHOICES['STATUS'])
