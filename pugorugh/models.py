6fdbce83bc39a8ca5d5702f329f917b620907391
CrossroadsMan <crossroadsman@gmail.com>
2019-02-01 10:55:14 -0700

    Update Pugorugh models

--- pugorugh/models.py
+++ pugorugh/models.py
@@ -4,60 +4,78 @@ from django.db.models import (CharField, PositiveIntegerField, ForeignKey)
 
 
 class Dog(Model):
-    CHOICES = {
-        'GENDER': (
-            ('m', 'male'),
-            ('f', 'female'),
-            ('u', 'unknown'),
-        ),
-        'SIZE': (
-            ('s': 'small'),
-            ('m': 'medium'),
-            ('l': 'large'),
-            ('xl': 'extra large'),
-            ('u': 'unknown'),
-        ),
-    }
+    MALE = 'm'
+    FEMALE = 'f'
+    UNKNOWN = 'u'
+    GENDER_CHOICES = (
+        (MALE, 'male'),
+        (FEMALE, 'female'),
+        (UNKNOWN, 'unknown'),
+    )
+    SMALL = 's'
+    MEDIUM = 'm'
+    LARGE = 'l'
+    EXTRA_LARGE = 'xl'
+    SIZE_CHOICES = (
+        (SMALL, 'small'),
+        (MEDIUM, 'medium'),
+        (LARGE, 'large'),
+        (EXTRA_LARGE, 'extra large'),
+        (UNKNOWN, 'unknown'),
+    )
 
     name = CharField(max_length=255)
     # most Linux file systems are limited to 255 + 4096, Windows API: 260
     image_filename = CharField(max_length=255)
     breed = CharField(max_length=255)
     age = PositiveIntegerField()  # months
-    gender = CharField(max_length=1, choices=CHOICES['GENDER'])
-    size = CharField(max_length=2, choices=CHOICES['SIZE'])
+    gender = CharField(max_length=1, choices=GENDER_CHOICES)
+    size = CharField(max_length=2, choices=SIZE_CHOICES)
 
 
-class UserMTM(Model):
-    CHOICES = {
-        'STATUS': (
-            ('l', 'liked'),
-            ('d', 'disliked'),
-        ),
-        'AGE': (
-            ('b', 'baby'),
-            ('y', 'young'),
-            ('a', 'adult'),
-            ('s', 'senior'),
-        ),
-        'GENDER': (
-            ('m', 'male'),
-            ('f', 'female'),
-        ),
-        'SIZE': (
-            ('s', 'small'),
-            ('m', 'medium'),
-            ('l', 'large'),
-            ('xl', 'extra large'),
-        )
-    }
+class UserDog(Model):
 
-    class Meta:
-        abstract = True
-
-
-class UserDog(UserMTM):
-
+    LIKED = 'l'
+    DISLIKED = 'd'
+    STATUS_CHOICES = (
+        (LIKED, 'liked'),
+        (DISLIKED, 'disliked'),
+    )
     user = ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=CASCADE)
     dog = ForeignKey(to='Dog', on_delete=CASCADE)
-    status = CharField(max_length=1, choices=CHOICES['STATUS'])
+    status = CharField(max_length=1, choices=STATUS_CHOICES)
+
+
+class UserPref(Model):
+
+    BABY = 'b'
+    YOUNG = 'y'
+    ADULT = 'a'
+    SENIOR = 's'
+    AGE_CHOICES = (
+        (BABY, 'baby'),
+        (YOUNG, 'young'),
+        (ADULT, 'adult'),
+        (SENIOR, 'senior'),
+    )
+    MALE = 'm'
+    FEMALE = 'f'
+    GENDER_CHOICES = (
+        (MALE, 'male'),
+        (FEMALE, 'female'),
+    )
+    SMALL = 's'
+    MEDIUM = 'm'
+    LARGE = 'l'
+    EXTRA_LARGE = 'xl'
+    SIZE_CHOICES = (
+        (SMALL, 'small'),
+        (MEDIUM, 'medium'),
+        (LARGE, 'large'),
+        (EXTRA_LARGE, 'extra large'),
+    )
+
+    user = ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    age = CharField(max_length=1, choices=AGE_CHOICES)
+    gender = CharField(max_length=1, choices=GENDER_CHOICES)
+    size = CharField(max_length=2, choices=SIZE_CHOICES)
