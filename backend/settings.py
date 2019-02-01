d86cd8e2e0285762d97155a4241227d48ba8a89e
CrossroadsMan <crossroadsman@gmail.com>
2019-02-01 00:40:37 -0700

    Basic reimplementation of base app

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -53,6 +53,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'pugorugh',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +138,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATICFILES_DIR,
+]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
--- pugorugh/views.py
+++ pugorugh/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView
+
+from . import serializers
+
+
+class UserRegisterView(CreateAPIView):
+    
+    # Override the restrictive permissions specified in settings.py
+    permission_classes = (permissions.AllowAny,)
+    
+    model = get_user_model()
+    serializer_class = serializers.UserSerializer
