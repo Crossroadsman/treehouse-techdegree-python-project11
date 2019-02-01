a836fbd72165d04553e5ace29e9367d16c6e5db6
CrossroadsMan <crossroadsman@gmail.com>
2019-02-01 10:55:33 -0700

    Create initial serializers

--- pugorugh/serializers.py
+++ pugorugh/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
+from . import models
+
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -18,3 +20,37 @@ class UserSerializer(serializers.ModelSerializer):
         
         fields = ('username', 'password')
         model = get_user_model()
+
+
+class DogSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Dog
+        fields = (
+            'id',
+            'name',
+            'image_filename',
+            'breed',
+            'age',
+            'gender',
+            'size',
+        )
+
+
+class UserPrefSerializer(serializers.ModelSerializer):
+    user = ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    age = CharField(max_length=1, choices=AGE_CHOICES)
+    gender = CharField(max_length=1, choices=GENDER_CHOICES)
+    size = CharField(max_length=2, choices=SIZE_CHOICES)
+
+    class Meta:
+        model = models.UserPref
+        fields = (
+            'id',
+            ### TODO: REMOVE THIS WHEN DONE TESTING ###
+            'user'
+            ### TODO: REMOVE THIS WHEN DONE TESTING ###
+            'age',
+            'gender',
+            'size',
+        )
