af4f28a4f87bd56bf3c71378c09f225153758a25
CrossroadsMan <crossroadsman@gmail.com>
2019-02-04 17:52:32 -0700

    Create working UserPrefs view

--- pugorugh/serializers.py
+++ pugorugh/serializers.py
@@ -13,6 +13,10 @@ class UserSerializer(serializers.ModelSerializer):
             username=validated_data['username']
         )
         user.set_password(validated_data['password'])
+
+        # create associated UserPref object
+        models.UserPref.objects.create(user=user)
+
         user.save()
         return user
 
@@ -37,15 +41,77 @@ class DogSerializer(serializers.ModelSerializer):
         )
 
 
+# A serializer's `save` method will either create a new instance or update
+# an existing instance, depending on whether an existing instance was
+# passed when instantiating the serializer.
+#
+# You can pass additional attributes to save() (e.g., the current user):
+# ```python
+# serializer.save(owner=request.user)
+# ```
+#
+# Any additional kwargs will be included in the validated_data arg when
+# .create or .update are called
+#
+# You can also pass context info to the serializer on instantiation:
+# serializer = serializers.UserPrefSerializer(data=request_data, context={'user': user})
 class UserPrefSerializer(serializers.ModelSerializer):
+
+    # Custom Field-Level Validation
+    # -----------------------------
+    def validate_age(self, value):
+        valid_ages = ['b', 'y', 'a', 's']
+        age_components = value.split(",")
+        for age in age_components:
+            if age not in valid_ages:
+                raise serializers.ValidationError("Invalid character in ages")
+        age_set = set(age_components)
+        if len(age_components) != len(age_set):
+            raise serializers.ValidationError("Repeated character in ages")
+        return value
+
+    def validate_gender(self, value):
+        valid_genders = ['m', 'f']
+        gender_components = value.split(",")
+        for gender in gender_components:
+            if gender not in valid_genders:
+                raise serializers.ValidationError("Invalid character in genders")
+        gender_set = set(gender_components)
+        if len(gender_components) != len(gender_set):
+            raise serializers.ValidationError("Repeated character in genders")
+        return value
+
+    def validate_size(self, value):
+        valid_sizes = ['s', 'm', 'l', 'xl']
+        size_components = value.split(",")
+        for size in size_components:
+            if size not in valid_sizes:
+                raise serializers.ValidationError("Invalid character in sizes")
+        size_set = set(size_components)
+        if len(size_components) != len(size_set):
+            raise serializers.ValidationError("Repeated character in sizes")
+        return value
+
+    def create(self, validated_data):
+        # see https://www.django-rest-framework.org/api-guide/validators/#advanced-field-defaults
+        # "request must have been provided as part of the context dictionary when
+        # instantiating the serializer"
+        # pass user into save -> it comes through as an element in
+        # validated_data
+        return models.UserPref.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.user = instance.user  # a user can't change another user's
+                                       # user association
+        instance.age = validated_data.get('age', instance.age)
+        instance.gender = validated_data.get('gender', instance.gender)
+        instance.size = validated_data.get('size', instance.size)
+        instance.save()
+        return instance
 
     class Meta:
         model = models.UserPref
         fields = (
-            'id',
-            ### TODO: REMOVE ↓↓↓↓ WHEN DONE TESTING ###
-            'user',
-            ### TODO: REMOVE ↑↑↑↑ WHEN DONE TESTING ###
             'age',
             'gender',
             'size',
