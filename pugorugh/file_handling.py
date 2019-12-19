fe463463aaf514a098816563c3fc1c5dacb12aa1
CrossroadsMan <crossroadsman@gmail.com>
2019-12-19 15:44:51 -0700

    Fix file_handling to write to specified directory

--- pugorugh/file_handling.py
+++ pugorugh/file_handling.py
@@ -52,7 +52,7 @@ def process_upload(file, pk, upload_dir=settings.DOG_UPLOAD_DIR):
     new_name = rename(file.name, pk)
 
     # 2
-    handle_uploaded_file(file, settings.DOG_UPLOAD_DIR, new_name)
+    handle_uploaded_file(file, upload_dir, new_name)
 
     # 3
     return new_name
